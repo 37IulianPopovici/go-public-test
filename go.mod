65fb937f087fd8656be02999d682cd63224871ab
37IulianPopovici <iulian.popovici@snyk.io>
2024-12-04 13:46:09 +0200

    fgsdghfs

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.21.4
+
+require github.com/37IulianPopovici/go-private-test v0.0.3
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	simpleSum "github.com/37IulianPopovici/go-private-test"
+)
+
+func main() {
+	fmt.Print(simpleSum.Sum(1, 2))
+}
